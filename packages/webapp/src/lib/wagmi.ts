bd913c245c1f987125d9dd854aef30aa9a24240d
MacCar <534192336car@gmail.com>
2025-07-22 12:21:32 +0800

    ✨ feat(支持 anvil):

--- packages/webapp/src/lib/wagmi.ts
+++ packages/webapp/src/lib/wagmi.ts
@@ -1,12 +1,16 @@
+/*
+ * @Author: Mr.Car
+ * @Date: 2025-07-16 15:26:57
+ */
 'use client';
 
 import { createConfig, http } from 'wagmi';
-import { sepolia, mainnet } from 'wagmi/chains';
+import { sepolia, mainnet, anvil } from 'wagmi/chains';
 import { injected, walletConnect } from 'wagmi/connectors';
 
 // 简化版配置（不需要 Project ID）
 export const config = createConfig({
-  chains: [sepolia, mainnet],
+  chains: [sepolia, mainnet, anvil],
   connectors: [
     injected(),
     // walletConnect({ projectId: 'YOUR_PROJECT_ID' }), // 暂时注释掉
@@ -14,6 +18,7 @@ export const config = createConfig({
   transports: {
     [sepolia.id]: http(),
     [mainnet.id]: http(),
+    [anvil.id]: http(),
   },
 });
 
